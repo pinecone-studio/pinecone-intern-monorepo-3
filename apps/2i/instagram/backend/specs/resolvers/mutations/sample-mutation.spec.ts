5011942741cd9605728728c5f073076dab04d87e
25LP7473 25LP7473 <25LP7473@administrators-iMac.local>
2025-08-20 16:39:35 +0800

    feat: backend of login or signup

--- apps/2i/instagram/backend/specs/resolvers/mutations/sample-mutation.spec.ts
+++ apps/2i/instagram/backend/specs/resolvers/mutations/sample-mutation.spec.ts
@@ -1,7 +1,7 @@
 import { sampleMutation } from '../../../src/resolvers/mutations';
 
 describe('sampleMutation', () => {
-  it('should return "Hello sample mutation"', () => {
+  it('should return "Hello sample mutation', () => {
     const result = sampleMutation();
     expect(result).toBe('Hello sample mutation');
   });
