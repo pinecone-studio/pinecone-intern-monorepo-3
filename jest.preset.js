8527b1fa0ab288ef00e6b271b2a84755eb031360
Zolo <zolbayar.in@gmail.com>
2025-08-20 15:14:43 +0800

    Update jest.preset.js

--- jest.preset.js
+++ jest.preset.js
@@ -4,4 +4,12 @@ module.exports = {
   ...nxPreset,
   snapshotFormat: { escapeString: true, printBasicPrototype: true },
   coverageReporters: ['text', 'html'],
+    coverageThreshold: {	
+    global: {	
+      branches: 100,	
+      functions: 100,	
+      lines: 100,	
+      statements: 100,	
+    },	
+  },
 };
