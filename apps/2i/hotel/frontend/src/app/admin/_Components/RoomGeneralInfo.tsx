241f990816f373aaab95862754ee7c16d811119c
odgarag <odgarag.odko@gmail.com>
2025-08-21 13:38:14 +0800

    .

--- apps/2i/hotel/frontend/src/app/admin/_Components/RoomGeneralInfo.tsx
+++ apps/2i/hotel/frontend/src/app/admin/_Components/RoomGeneralInfo.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
