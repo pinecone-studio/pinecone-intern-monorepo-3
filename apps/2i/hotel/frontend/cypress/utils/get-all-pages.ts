2637d5f95b6fea216d66725ec2c7c03f93c1bd56
Khuvituguldur <macintosh@KhuiZayas-MacBook-Air.local>
2025-08-22 14:38:57 +0800

    fix(hotel-frontend): change 2I => 2i

--- apps/2i/hotel/frontend/cypress/utils/get-all-pages.ts
+++ apps/2i/hotel/frontend/cypress/utils/get-all-pages.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const root = './apps/2I/hotel/frontend/src/app';
+const root = './apps/2i/hotel/frontend/src/app';
 
 const searchDepth = (folderPath: string) => {
   let pages: string[] = [];
