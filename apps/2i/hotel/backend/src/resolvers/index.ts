33ca88d415f4a21fd35fbdfdfb0094f8f1dcfd13
Khuvituguldur <macintosh@KhuiZayas-MacBook-Air.local>
2025-08-22 16:42:17 +0800

    gg

--- apps/2i/hotel/backend/src/resolvers/index.ts
+++ apps/2i/hotel/backend/src/resolvers/index.ts
@@ -4,15 +4,16 @@ import * as Query from './queries';
 export const resolvers = {
   Mutation: {
     addRoom: Mutation.addRoom,
-     addHotel: Mutation.addHotel,
+    addHotel: Mutation.addHotel,
     updateHotel: Mutation.updateHotel,
-    deleteHotel:Mutation.deleteHotel,
-   submitUserRating: Mutation.submitUserRating
+    deleteHotel: Mutation.deleteHotel,
+    submitUserRating: Mutation.submitUserRating,
   },
   Query: {
     getRoomById: Query.getRoomById,
     getRooms: Query.getRooms,
-       getHotel: Query.getHotel,
-   getHotelById:Query.getHotelById
+    getHotel: Query.getHotel,
+    getHotelById: Query.getHotelById,
+    getAvailableRooms: Query.getAvailableRooms,
   },
 };
