58a414ad0a854216228071e9bbe941e4f2dfa765
Khuvituguldur <macintosh@KhuiZayas-MacBook-Air.local>
2025-08-22 14:05:30 +0800

    fix(hotel-frontend): change name of RoomDetail=>RoomDetails

--- apps/2i/hotel/frontend/src/app/admin/_Components/GuestsPage.tsx
+++ apps/2i/hotel/frontend/src/app/admin/_Components/GuestsPage.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { RoomDetail } from './RoomDetails';
+import { RoomDetails } from './RoomDetails';
 
 export const GuestsPage = () => {
   const [statusFilter, setStatusFilter] = useState('');
@@ -68,7 +68,7 @@ export const GuestsPage = () => {
     }
   };
   if (selectedRoomId) {
-    return <RoomDetail roomId={selectedRoomId} />;
+    return <RoomDetails roomId={selectedRoomId} />;
   }
 
   return (
