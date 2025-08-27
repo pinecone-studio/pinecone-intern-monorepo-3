f85b5e9e87b938930b432c020e9a00c6ece7e806
25LP0080 25LP0080 <25LP0080@Admins-iMac-4.local>
2025-08-27 12:42:30 +0800

    feat(frontend): change frontend commit

---
+++ apps/2i/hotel/frontend/src/app/admin/_Components/DetailsImage.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Label } from '@/components/ui/label';
+import { ImageOff, Plus } from 'lucide-react';
+import Image from 'next/image';
+
+export const DetailImage = () => {
+  const [images, setImages] = useState<string[]>([]);
+
+  return (
+    <Card className="pt-4 pr-6 pb-6 pl-6">
+      <div className="flex items-center justify-between ">
+        <h2 className="text-lg font-semibold text-gray-900">Images</h2>
+
+        <Dialog>
+          <DialogTrigger className="text-[#2563EB]">Edit</DialogTrigger>
+          <DialogContent className="w-[1160px] max-h-[800px] overflow-y-auto">
+            <DialogHeader>
+              <DialogTitle>Images</DialogTitle>
+            </DialogHeader>
+
+            {/* Upload хэсэг */}
+            <div className="grid grid-cols-2 gap-6 mb-6">
+              <Label htmlFor="image-upload" className="w-[552px] h-[300px] cursor-pointer border-2 border-dashed rounded-md p-8 text-sm flex flex-col justify-center gap-2 items-center   bg-gray-50 ">
+                <Plus className="w-8 h-8 text-[#2563EB]" />
+                <span>Drag or Upload Photo</span>
+                <input
+                  id="image-upload"
+                  type="file"
+                  className="hidden"
+                  onChange={(e) => {
+                    if (e.target.files && e.target.files[0]) {
+                      const url = URL.createObjectURL(e.target.files[0]);
+                      setImages((prev) => [...prev, url]); // state-д нэмэх
+                    }
+                  }}
+                />
+              </Label>
+            </div>
+
+            <div className="flex justify-between">
+              <Button variant="ghost">Cancel</Button>
+              <Button className="bg-[#2563EB] text-white">Save</Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <CardContent className="mt-4">
+        <div className="flex flex-col gap-1">
+          {images.length > 0 ? (
+            // Зураг байвал gallery
+            <div>
+              <div className="w-full h-64">
+                <Image src={images[0]} alt="Main" width={625} height={400} className="w-full h-full object-cover rounded-xl border shadow-md" />
+              </div>
+
+              <div className="grid grid-cols-2 gap-3">
+                {images.slice(1).map((img, i) => (
+                  <Image key={i} src={img} alt={`Uploaded ${i}`} width={250} height={180} className="h-44 w-full object-cover rounded-lg border shadow hover:scale-105 transition" />
+                ))}
+              </div>
+            </div>
+          ) : (
+            // Хоосон төлөв
+            <div className="flex flex-col items-center justify-center  rounded-lg text-center ">
+              <div className="font-medium flex flex-col gap-3 justify-center items-center">
+                <ImageOff className="text-muted-foreground" />
+                <p>No Photos Uploaded</p>
+              </div>
+              <p className="text-sm text-gray-400">Add photos of your rooms, amenities, or property to showcase your hotel.</p>
+            </div>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ apps/2i/hotel/frontend/src/app/admin/_Components/RoomTypes.tsx
@@ -0,0 +1,69 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Plus } from 'lucide-react';
+import Image from 'next/image';
+
+export const RoomTypes = () => {
+  return (
+    <Card className="bg-white p-6 rounded-lg shadow-sm">
+      <Table>
+        <div className=" flex justify-between ">
+          <div className="mb-4">
+            <h1 className="text-lg font-semibold text-gray-800">Room Types</h1>
+          </div>
+          <Button variant="ghost" className="text-[#2563EB]">
+            {' '}
+            <Plus />
+            Add Room
+          </Button>
+        </div>
+
+        <CardContent>
+          <Tabs defaultValue="All" className="w-full">
+            <TabsList className="mb-4">
+              <TabsTrigger value="All">All Rooms</TabsTrigger>
+              <TabsTrigger value="one-bed">1 Bed</TabsTrigger>
+              <TabsTrigger value="two-bed">2 Bed</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="All">
+              <div className="rounded-lg overflow-hidden border">
+                <Table>
+                  <TableHeader className="bg-gray-100">
+                    <TableRow>
+                      <TableHead className="w-[82px]">ID</TableHead>
+                      <TableHead>Name</TableHead>
+                      <TableHead>Price</TableHead>
+                      <TableHead className="text-right">Bed</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow className="hover:bg-gray-50">
+                      <TableCell className="font-medium">0001</TableCell>
+                      <TableCell className="flex items-center gap-2 text-sm">
+                        <Image src="/path/to/image.jpg" alt="Room Image" width={40} height={40} className="rounded-md object-cover" />
+                        Economy Single room
+                      </TableCell>
+                      <TableCell>$250.00</TableCell>
+                      <TableCell className="text-right">1 Bed</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="one-bed">
+              <p className="text-gray-500">1 bed room list goes here...</p>
+            </TabsContent>
+
+            <TabsContent value="two-bed">
+              <p className="text-gray-500">2 bed room list goes here...</p>
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Table>
+    </Card>
+  );
+};
