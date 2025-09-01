f99245aa9d9a5a457041e9adde6e742ae88e07dc
batbayar <baabar@batbayars-MacBook-Pro.local>
2025-09-01 11:39:57 +0800

    feat(backend): get booking

--- apps/2i/hotel/backend/src/resolvers/queries/get-booking-query.ts
+++ apps/2i/hotel/backend/src/resolvers/queries/get-booking-query.ts
@@ -2,7 +2,7 @@ import { BookingModel } from '../../models/room-booking-model';
 
 export const getBooking = async (
   _: unknown,
-  args: { userId: string; hotelName: string; roomNumber: string; checkIn: Date; checkOut: Date; nights: Number; pricePerNight: Number; taxes: Number; totalPrice: Number }
+  args: { userId: string; hotelName: string; roomNumber: string; checkIn: Date; checkOut: Date; nights: number; pricePerNight: number; taxes: number; totalPrice: number }
 ) => {
   const existingBooking = await BookingModel.findOne({ userId: args.userId, hotelName: args.hotelName, roomNumber: args.roomNumber, checkIn: args.checkIn, checkOut: args.checkOut })
     .populate('hotel')
