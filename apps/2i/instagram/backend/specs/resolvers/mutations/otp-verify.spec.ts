1e73bcae335db2a381409340994d0b6a0f0bf36f
bumbarai <bumbarai.t@gmail.com>
2025-09-03 15:19:46 +0800

    aldaa zasav

--- apps/2i/instagram/backend/specs/resolvers/mutations/otp-verify.spec.ts
+++ apps/2i/instagram/backend/specs/resolvers/mutations/otp-verify.spec.ts
@@ -1,57 +1,49 @@
 process.env.JWT_SECRET = 'supersecret';
-import { verifyOtp } from "../../../src/resolvers/mutations/otp-verify";
-import { OtpModel } from "../../../src/models/otpmodel";
-import { UserModel } from "../../../src/models";
-import jwt from "jsonwebtoken";
+import { verifyOtp } from '../../../src/resolvers/mutations/otp-verify';
+import { OtpModel } from '../../../src/models/otpmodel';
+import { UserModel } from '../../../src/models';
+import jwt from 'jsonwebtoken';
 
-jest.mock("../../../src/models/otpmodel");
-jest.mock("../../../src/models");
-jest.mock("jsonwebtoken");
+jest.mock('../../../src/models/otpmodel');
+jest.mock('../../../src/models');
+jest.mock('jsonwebtoken');
 
-describe("verifyOtp function", () => {
-  const mockOtp = "123456";
-  const mockUserId = "user123";
-  const mockEmail = "test@example.com";
+describe('verifyOtp function', () => {
+  const mockOtp = '123456';
+  const mockUserId = 'user123';
+  const mockEmail = 'test@example.com';
   const fakeDateNow = new Date();
 
   beforeEach(() => {
-    
     jest.clearAllMocks();
     jest.useFakeTimers().setSystemTime(fakeDateNow);
-    
   });
-
-   
 
   afterEach(() => {
     jest.useRealTimers();
   });
 
-  it("should throw error if OTP not found", async () => {
+  it('should throw error if OTP not found', async () => {
     (OtpModel.findOne as jest.Mock).mockReturnValue({
       sort: jest.fn().mockResolvedValue(null),
     });
 
-    await expect(verifyOtp(null, { verifyOtp: { otp: mockOtp } })).rejects.toThrow(
-      "OTP код олдсонгүй"
-    );
+    await expect(verifyOtp(null, { verifyOtp: { otp: mockOtp } })).rejects.toThrow('OTP код олдсонгүй');
   });
 
-  it("should throw error if OTP expired", async () => {
+  it('should throw error if OTP expired', async () => {
     (OtpModel.findOne as jest.Mock).mockReturnValue({
       sort: jest.fn().mockResolvedValue({
         code: mockOtp,
-        expiresAt: new Date(fakeDateNow.getTime() - 1000), 
+        expiresAt: new Date(fakeDateNow.getTime() - 1000),
         userId: mockUserId,
       }),
     });
 
-    await expect(verifyOtp(null, { verifyOtp: { otp: mockOtp } })).rejects.toThrow(
-      "OTP кодын хугацаа дууссан байна"
-    );
+    await expect(verifyOtp(null, { verifyOtp: { otp: mockOtp } })).rejects.toThrow('OTP кодын хугацаа дууссан байна');
   });
 
-  it("should throw error if user not found", async () => {
+  it('should throw error if user not found', async () => {
     (OtpModel.findOne as jest.Mock).mockReturnValue({
       sort: jest.fn().mockResolvedValue({
         code: mockOtp,
@@ -61,13 +53,10 @@ describe("verifyOtp function", () => {
     });
     (UserModel.findById as jest.Mock).mockResolvedValue(null);
 
-    await expect(verifyOtp(null, { verifyOtp: { otp: mockOtp } })).rejects.toThrow(
-      "Хэрэглэгч олдсонгүй"
-    );
+    await expect(verifyOtp(null, { verifyOtp: { otp: mockOtp } })).rejects.toThrow('Хэрэглэгч олдсонгүй');
   });
 
-  it("should activate user, save and return token and users", async () => {
-  
+  it('should activate user, save and return token and user', async () => {
     const mockUser = {
       id: mockUserId,
       email: mockEmail,
@@ -84,26 +73,25 @@ describe("verifyOtp function", () => {
     });
     (UserModel.findById as jest.Mock).mockResolvedValue(mockUser);
 
-    (jwt.sign as jest.Mock).mockReturnValue("mocked_jwt_token");
+    (jwt.sign as jest.Mock).mockReturnValue('mocked_jwt_token');
 
     const result = await verifyOtp(null, { verifyOtp: { otp: mockOtp } });
 
-expect(jwt.sign).toHaveBeenCalledWith(
-  expect.objectContaining({
-    userId: mockUser.id,
-    email: mockUser.email,
-  }),
- 'supersecret',
-  expect.objectContaining({
-    expiresIn: "7d"
-  })
-);
+    expect(jwt.sign).toHaveBeenCalledWith(
+      expect.objectContaining({
+        userId: mockUser.id,
+        email: mockUser.email,
+      }),
+      'supersecret',
+      expect.objectContaining({
+        expiresIn: '7d',
+      })
+    );
 
     expect(result).toEqual({
-      message: "OTP баталгаажуулалт амжилттай",
-      token: "mocked_jwt_token",
+      message: 'OTP баталгаажуулалт амжилттай',
+      token: 'mocked_jwt_token',
       user: mockUser,
     });
   });
 });
-
