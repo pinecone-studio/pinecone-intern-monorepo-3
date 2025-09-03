d3061a93644554244885c0e21688e6f26f7dd9b7
sumiyab <sumiyabazaramarjargal@gmail.com>
2025-09-03 14:39:01 +0800

    feat: setup 2j projects

---
+++ apps/2j/real-state/frontend/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+resource "vercel_project" "state-frontend" {
+  name             = "state-frontend"
+  build_command    = "nx build --skip-nx-cache state-frontend"
+  output_directory = "./dist/apps/2j/real-state/frontend/.next"
+  framework        = "nextjs"
+  team_id          = "team_0ASDilhqwPl5fll9OnzqDM30"
+}
+resource "vercel_project" "state-frontend" {
+  name             = "state-frontend"
+  build_command    = "nx build --skip-nx-cache state-frontend"
+  output_directory = "./dist/apps/2j/real-state/frontend/.next"
+  framework        = "nextjs"
+  team_id          = "team_0ASDilhqwPl5fll9OnzqDM30"
+}
+resource "vercel_project" "state-frontend" {
+  name             = "state-frontend"
+  build_command    = "nx build --skip-nx-cache state-frontend"
+  output_directory = "./dist/apps/2j/real-state/frontend/.next"
+  framework        = "nextjs"
+  team_id          = "team_0ASDilhqwPl5fll9OnzqDM30"
+}
+
+variable "VERCEL_TOKEN" {
+  type        = string
+  description = "Optionally say something about this variable"
+}
+
+provider "vercel" {
+  # Or omit this for the api_token to be read
+  # from the VERCEL_API_TOKEN environment variable
+  api_token = var.VERCEL_TOKEN
+
+  # Optional default team for all resources
+  team = "team_0ASDilhqwPl5fll9OnzqDM30"
+}
