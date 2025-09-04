c65fcc4208411173f4aab680c1b477dbf21b27c6
NaTsO2 <narantsogt.md@gmail.com>
2025-09-04 12:46:53 +0800

    fix(frontend): fix

--- apps/2i/hotel/frontend/src/app/admin/hotels/[id]/page.tsx
+++ apps/2i/hotel/frontend/src/app/admin/hotels/[id]/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { useParams } from 'next/navigation';
-import { HotelDetail } from './_Components/HotelDetail';
+// import { useParams } from 'next/navigation';
+// import { HotelDetail } from './_Components/HotelDetail';
 
 const HotelHomePage = () => {
-  const { hotelId } = useParams() as { hotelId: string };
+  // const { hotelId } = useParams() as { hotelId: string };
 
-  return (
-    <div>
-      <HotelDetail hotelId={hotelId} />
-    </div>
-  );
+  return <div>{/* <HotelDetail hotelId={hotelId} /> */}</div>;
 };
 export default HotelHomePage;
