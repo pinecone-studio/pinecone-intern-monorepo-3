9df7f1f0387db1991c10c34394a93269bfa7ce45
Bolormaa <boloroots47@gmail.com>
2025-10-16 19:03:56 +0800

    feat(backend): backend Food CRUD, FoodOrder CRUD

--- apps/2k/restaurant/backend/src/resolvers/mutations/index.ts
+++ apps/2k/restaurant/backend/src/resolvers/mutations/index.ts
@@ -1,14 +1,12 @@
 export * from './sample-mutation';
+
 export * from './food/create-food';
 export * from './food/update-food';
 export * from './food/delete-food';
 
 export * from './foodOrder/create-food-order';
+
 export * from './user/create-user';
-export * from './auth/login';
 export * from './user/update-user';
-export * from './food/create-food';
-export * from './food/update-food';
-export * from './food/delete-food';
 
-export * from './foodOrder/create-food-order';
+export * from './auth/login';
