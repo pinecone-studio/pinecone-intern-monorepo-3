9ca4917db5a11b8a97bc677e4e1fb33fef7dad0a
Tugstuguldur <lelouch.cc.28@gmail.com>
2025-10-17 13:31:48 +0800

    apply stashed changes after rebase

--- apps/2j/concert-ticket/frontend/src/app/(auth)/sign-in/page.tsx
+++ apps/2j/concert-ticket/frontend/src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,5 @@
-import { SquareChevronLeft, CheckCircle } from 'lucide-react';
+'use client';
+
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -54,5 +55,31 @@ const Page = () => {
       alert(err.message);
     }
   };
-  return <div>signin</div>;
+  return (
+    <div className="bg-black w-screen h-screen justify-center items-center text-white flex">
+      <div className="w-1/3 h-1/2 border border-neutral-600 rounded-md flex p-12 flex-col gap-10">
+        <h1 className="flex justify-center w-full font-bold text-[24px]">Нэвтрэх</h1>
+        <div className="flex-col gap-6 flex justify-between h-full">
+          <div className="flex-col gap-3 flex">
+            <p>Имэйл хаяг:</p>
+            <input type="email" placeholder="name@example.com" className="w-full bg-black border border-neutral-600 rounded-md py-2 px-4" />
+          </div>
+          <div className="flex-col gap-3 flex">
+            <p>Нууц үг:</p>
+            <input type="password" className="w-full bg-black border border-neutral-600 rounded-md py-2 px-4" />
+          </div>
+          <button className="bg-blue-400 text-black rounded-lg py-3">Нэвтрэх</button>
+          <p className="w-full flex justify-center text-neutral-400">
+            Та бүртгэлтэй хаяггүй бол
+            <Link href="/sign-up" className="underline mx-1 hover:text-neutral-200">
+              бүртгүүлэх
+            </Link>
+            хэсгээр орно уу
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Page;
