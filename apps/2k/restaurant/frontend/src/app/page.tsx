1d3490088f4dfb3fbf6b9d905df1ed626d77e1e2
25LP3742 25LP3742 <25LP3742@administrators-iMac-3.local>
2025-10-20 14:35:05 +0800

    feat(frontend): homepage

--- apps/2k/restaurant/frontend/src/app/page.tsx
+++ apps/2k/restaurant/frontend/src/app/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+<<<<<<< Updated upstream
 import SignInPage from './(private)/sign-in/page';
 
 const Page = () => {
   return (
     <div>
       <SignInPage />
+=======
+import { HomePage } from "@/components/home/HomePage";
+
+const Page = () => {
+  return(
+    <div>
+      <HomePage/>
+>>>>>>> Stashed changes
     </div>
   );
 };
